7dbc325b7e1636203d05f2cf3d4cb26e0f3fbcc1
Fox-R-fox <rohansherkar2207@gmail.com>
2024-12-19 21:06:47 +0530

    Update backend.tf

--- EKS-TF/backend.tf
+++ EKS-TF/backend.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "s3" {
-    bucket         = "builddb"
+    bucket         = "flappybird-terraform-state"
     region         = "us-east-1"
     key            = "End-to-End-Kubernetes-DevSecOps-Tetris-Project/EKS-TF/terraform.tfstate"
     dynamodb_table = "Lock-Files"
